dfd9c126e1edc8feeaf3e0d37c2b24226b9f8317
tajOpti <tajbiul.rawol@optimizely.com>
2024-07-11 20:21:16 +0600

    add routing

--- e-store/src/components/Category.jsx
+++ e-store/src/components/Category.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Category = ({ id, title, onCategoryClick }) => {
     return (
-        <div onClick={() => onCategoryClick(id)} key={id}>{title}</div>
+        <div className='cat-item' onClick={() => onCategoryClick(id)} key={id}>{title}</div>
     )
 }
 
