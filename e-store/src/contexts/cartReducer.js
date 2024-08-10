7e4d95d2fc383237a985ee85e9f57330562b433d
tajOpti <tajbiul.rawol@optimizely.com>
2024-08-10 16:10:07 +0600

    add more method to the context and handle more states for product handling in the reducer

--- e-store/src/contexts/cartReducer.js
+++ e-store/src/contexts/cartReducer.js
@@ -1,17 +1,34 @@
 export const CartReducer = (state, action) => {
     debugger;
+    let index = -1;
+    if (action.payload) {
+        index = state.cartItems.findIndex(x => x.id === action.payload.id);
+    }
     switch (action.type) {
         case 'ADD':
-            const index = state.cartItems.findIndex(x => x.id === action.payload.id);
+        case 'INCQTY':
             if (index === -1) {
                 state.cartItems.push({ ...action.payload, quantity: 1 });
             } else {
                 state.cartItems[index].quantity += 1;
             }
-            return state;
+            break;
         case 'REMOVE':
-            return { ...state, cart: [...state.cart, action.payload] };
+            if (index > -1) {
+                state.cartItems.splice(index, 1);
+            }
+            break;
+        case 'DECQTY':
+            if (index > -1) {
+                state.cartItems[index].quantity--;
+            }
+            break;
+        case 'CLEAR':
+            state.cartItems = [];
+            break;
         default:
-            return state;
+            break;
+
     }
+    return state;
 }
